a492201333b3aa1c7708549f9c4368cd739c61cd
AnnaLipetska <a.lipetska@gmail.com>
2020-05-05 19:18:14 +0300

    OK or not OK

--- src/main/java/lesson2/Main.java
+++ src/main/java/lesson2/Main.java
@@ -3,7 +3,7 @@ package main.java.lesson2;
 public class Main {
     public static void main(String[] args) {
         Task1 anna = new Task1();
-        Task1 bobik = new Task1 ("bobik", 5);
+        Task1 bobik = new Task1("bobik", 5);
         anna.setName("anna");
         anna.setAge(18);
         System.out.println(anna.getName() + " " + anna.getAge() + " " + anna.getAgeGroup());
