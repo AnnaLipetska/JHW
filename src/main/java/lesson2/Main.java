ac8964e5f87562d06a07a53b023fe423bce84721
AnnaLipetska <a.lipetska@gmail.com>
2020-05-05 18:44:30 +0300

    MB OK.

--- src/main/java/lesson2/Main.java
+++ src/main/java/lesson2/Main.java
@@ -1,4 +1,12 @@
 package main.java.lesson2;
 
 public class Main {
+    public static void main(String[] args) {
+        Task1 anna = new Task1();
+        Task1 bobik = new Task1 ("bobik", 5);
+        anna.setName("anna");
+        anna.setAge(18);
+        System.out.println(anna.getName() + " " + anna.getAge() + " " + anna.getAgeGroup());
+        System.out.println(bobik.getName() + " " + bobik.getAge() + " " + bobik.getAgeGroup());
+    }
 }
--- src/main/java/lesson2/Task1.java
+++ src/main/java/lesson2/Task1.java
@@ -2,57 +2,97 @@ package main.java.lesson2;
 
 public class Task1 {
     // Реализовать у класса Task1 переменные age (тип int),  name (тип String), ageGroup(тип String)
+    // не должно быть возможности напрямую устанавливать или читать значение age/name, поэтому private
     private int age;
     private String name;
     private String ageGroup;
 
+    // Конструктор
+    public Task1() {
+        this("not defined", 0);
+    }
+
+    public Task1(String name) {
+        this(name, 0);
+    }
+
+    public Task1(int age) {
+        this("not defined", age);
+    }
+
+    public Task1(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.ageGroup = getAgeGroup();
+    }
+
+
+    // реализовать методы геттеры и сеттеры для каждой переменной.
     public int getAge() {
         return this.age;
+    }
+
+    public void setAge(int age) { // максимальныое значение для возраста - 100
+        if (age >= 0 && age <= 100) {
+            this.age = age;
+        } else { // нельзя установить значение age меньше нуля
+            System.out.println("You can't set age " + age);
+            this.age = 0;
+        }
+        /**
+         *      - child если age до 15 лет
+         *      - student  - если age от 15 до 25 лет
+         *      - worker - если age от 26 до 65 лет
+         *      - pensioner - если age старше 66 лет
+         */
+        if (age < 15) {
+            ageGroup = "child";
+        } else if (age >= 15 && age <= 25) {
+            ageGroup = "student";
+        } else if (age >= 26 && age <= 65) {
+            ageGroup = "worker";
+        } else if (age >= 66) {  // или можно просто else без условия
+            ageGroup = "pensioner";
+        }
     }
 
     public String getName() {
         return this.name;
     }
 
+    public void setName(String name) { // значение name не может быть короче чем 3 символа и длиннее чем 50 символов
+        int nameLength = name.length();
+        if (name.length() >= 3 && name.length() <= 50 && name.matches("^\\S+")) {
+            name = name.substring(0, 1).toUpperCase() + name.substring(1);
+            // не зависимо от того ввел пользователь имя с большой или с маленькой буквы,
+            // оно должно быть записано в переменную name с большой буквы
+            this.name = name;
+        } else {
+            System.out.println("The length of name should be 3 to 50 symbols and to consist of only whitespaces.");
+            this.name = "not defined";
+        }
+    }
+
     public String getAgeGroup() {
+        /**     - child если age до 15 лет
+         *      - student  - если age от 15 до 25 лет
+         *      - worker - если age от 26 до 65 лет
+         *      - pensioner - если age старше 66 лет
+         */
+        if (age < 15) {
+            ageGroup = "child";
+        } else if (age >= 15 && age <= 25) {
+            ageGroup = "student";
+        } else if (age >= 26 && age <= 65) {
+            ageGroup = "worker";
+        } else if (age >= 66) {  // или можно просто else без условия
+            ageGroup = "pensioner";
+        }
         return this.ageGroup;
     }
 
-    public void setAge(int age) {
-
+    private void setAgeGroup(int age) {
+// ageGroup можно только прочитать с помощью геттера, сеттер должен быть приватным и недоступным для других классов
     }
+}
 
-    public void setName(String name) {
-
-    }
-
-    public void setAgeGroup(String ageGroup) {
-
-    }
-
-    /*public void setAge(int age) {
-       if (age > 0 && age < 20) {
-           this.age = age;
-       } else {
-           System.out.println("You can't set age " + age);
-           this.age = 0;
-       }
-   }
-*/
-    /**
-     * реализовать методы геттеры и сеттеры для каждой переменной.
-     * Должны быть выполненны следующие условия:
-     *  - не должно быть возможности напрямую устанваливать или читать значение age/name
-     *  - нельзя установить значение age меньше нуля
-     *  - максимальныое значение для возраста - 100
-     *  - значение name не может быть короче чем 3 символа и длиннее чем 50 символов
-     *  - name не может состоять из одних только пробелов
-     *  - не зависимо от того ввел пользователь имя с большой или с маленькой буквы,
-     *      оно должно быть записано в переменную name с большой буквы
-     *  - ageGroup должен устанавливаться автоматически при установке возраста
-     *      - child если age до 15 лет
-     *      - student  - если age от 15 до 25 лет
-     *      - worker - если age от 26 до 65 лет
-     *      - pensioner - если age старше 66 лет
-     *  - ageGroup можно только прочитать с помощью геттера, сеттер должен быть приватным и недоступным для других классов
-*/}
