377c0929db4e38996ddbd02b3c2c372fb22ae6e0
AnnaLipetska <a.lipetska@gmail.com>
2020-05-05 19:17:59 +0300

    OK or not OK

--- src/main/java/lesson2/Task1.java
+++ src/main/java/lesson2/Task1.java
@@ -21,9 +21,9 @@ public class Task1 {
     }
 
     public Task1(String name, int age) {
-        this.name = name;
+        this.name = CheckName(name);
         this.age = age;
-        this.ageGroup = getAgeGroup();
+        this.ageGroup = CalculateAgeGroup(age);
     }
 
 
@@ -39,41 +39,26 @@ public class Task1 {
             System.out.println("You can't set age " + age);
             this.age = 0;
         }
-        /**
-         *      - child если age до 15 лет
-         *      - student  - если age от 15 до 25 лет
-         *      - worker - если age от 26 до 65 лет
-         *      - pensioner - если age старше 66 лет
-         */
-        if (age < 15) {
-            ageGroup = "child";
-        } else if (age >= 15 && age <= 25) {
-            ageGroup = "student";
-        } else if (age >= 26 && age <= 65) {
-            ageGroup = "worker";
-        } else if (age >= 66) {  // или можно просто else без условия
-            ageGroup = "pensioner";
-        }
+        ageGroup = CalculateAgeGroup(age);
     }
 
     public String getName() {
         return this.name;
     }
 
-    public void setName(String name) { // значение name не может быть короче чем 3 символа и длиннее чем 50 символов
-        int nameLength = name.length();
-        if (name.length() >= 3 && name.length() <= 50 && name.matches("^\\S+")) {
-            name = name.substring(0, 1).toUpperCase() + name.substring(1);
-            // не зависимо от того ввел пользователь имя с большой или с маленькой буквы,
-            // оно должно быть записано в переменную name с большой буквы
-            this.name = name;
-        } else {
-            System.out.println("The length of name should be 3 to 50 symbols and to consist of only whitespaces.");
-            this.name = "not defined";
-        }
+    public void setName(String name) {
+        this.name = CheckName(name);
     }
 
     public String getAgeGroup() {
+        return this.ageGroup;
+    }
+
+    private void setAgeGroup(int age) {
+// ageGroup можно только прочитать с помощью геттера, сеттер должен быть приватным и недоступным для других классов
+    }
+
+    private String CalculateAgeGroup(int age) {
         /**     - child если age до 15 лет
          *      - student  - если age от 15 до 25 лет
          *      - worker - если age от 26 до 65 лет
@@ -91,8 +76,18 @@ public class Task1 {
         return this.ageGroup;
     }
 
-    private void setAgeGroup(int age) {
-// ageGroup можно только прочитать с помощью геттера, сеттер должен быть приватным и недоступным для других классов
+    private String CheckName(String name) {
+        // значение name не может быть короче чем 3 символа и длиннее чем 50 символов
+        int nameLength = name.length();
+        if (name.length() >= 3 && name.length() <= 50 && name.matches("^\\S+")) {
+            name = name.substring(0, 1).toUpperCase() + name.substring(1);
+            // не зависимо от того ввел пользователь имя с большой или с маленькой буквы,
+            // оно должно быть записано в переменную name с большой буквы
+        } else {
+            System.out.println("The length of name should be 3 to 50 symbols and to consist of only whitespaces.");
+            name = "not defined";
+        }
+        return name;
     }
 }
 
