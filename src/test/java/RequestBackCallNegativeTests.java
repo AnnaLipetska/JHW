31b13820e9da67f2e7f077a4b8164b5293bed5c6
AnnaLipetska <a.lipetska@gmail.com>
2020-05-18 22:34:28 +0300

    Assertions added

--- src/test/java/RequestBackCallNegativeTests.java
+++ src/test/java/RequestBackCallNegativeTests.java
@@ -14,6 +14,8 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.testng.Assert.assertEquals;
+
 public class RequestBackCallNegativeTests {
     WebDriver driver;
     WebDriverWait wait;
@@ -23,6 +25,11 @@ public class RequestBackCallNegativeTests {
     By username = By.xpath("//input[@formcontrolname='username']");
     By password = By.xpath("//input[@formcontrolname='password']");
     By greenButton = By.cssSelector("[class = 'button button_size_large button_color_green auth-modal__submit']");
+    String expectedColor = "rgb(248, 65, 71)";
+    String expectedGrey = "rgb(194, 194, 194)";
+    String actualNameColor;
+    String actualUsernameColor;
+    String actualPasswordColor;
 
 
     @BeforeMethod
@@ -32,20 +39,25 @@ public class RequestBackCallNegativeTests {
         options.addArguments("--disable-notifications");
         driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, 10);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("https://rozetka.com.ua/"); // Зайти на главную страницу https://rozetka.com.ua/
         waitClick(personal); // Нажать «войдите в личный кабинет»
         waitClick(register); // Нажать «Зарегестрироваться"
 
         // Кликнуть по полям Ваше имя, Эл. почта, Приудмайте пароль
-        waitClick(name);
+
         waitClick(username);
         waitClick(password);
+        waitClick(name);
     }
 
     public void waitClick(By elementToClick) {
         wait.until(ExpectedConditions.elementToBeClickable(elementToClick)).click();
+    }
+
+    public String getActualColor(By field) {
+        return driver.findElement(field).getCssValue("border-color");
     }
 
     @Test
@@ -53,6 +65,35 @@ public class RequestBackCallNegativeTests {
         // Оставить пустыми «Ваше имя», «Эл. Почта или номер телефона», «Придумайте Пароль»
         // Нажать «Войти»
         waitClick(greenButton);
+        // Проверить что незаполненные поля подкрашены красным цветом
+        waitClick(name);
+        waitClick(username);
+        waitClick(password);
+        actualNameColor = getActualColor(name);
+        actualUsernameColor = getActualColor(username);
+        actualPasswordColor = getActualColor(password);
+
+
+        assertEquals(
+                actualNameColor,
+                expectedColor,
+                "Expected color of the border of the field Ваше имя to be " + expectedColor +
+                        " but got " + actualNameColor
+        );
+
+        assertEquals(
+                expectedColor,
+                actualUsernameColor,
+                "Expected color of the border of the field Эл. почта to be " + expectedColor +
+                        " but got " + actualUsernameColor
+        );
+
+        assertEquals(
+                expectedColor,
+                actualPasswordColor,
+                "Expected color of the border of the field Придумайте пароль to be " + expectedColor +
+                        " but got " + actualPasswordColor
+        );
     }
 
     @Test
@@ -64,11 +105,39 @@ public class RequestBackCallNegativeTests {
         passwordField.sendKeys(Keys.ENTER);
         // Нажать «Войти»
         waitClick(greenButton);
+
+        waitClick(name);
+        waitClick(username);
+        waitClick(password);
+        actualNameColor = getActualColor(name);
+        actualUsernameColor = getActualColor(username);
+        actualPasswordColor = getActualColor(password);
+
+        assertEquals(
+                actualNameColor,
+                expectedColor,
+                "Expected color of the border of the field Ваше имя to be " + expectedColor +
+                        " but got " + actualNameColor
+        );
+
+        assertEquals(
+                expectedColor,
+                actualUsernameColor,
+                "Expected color of the border of the field Эл. почта to be " + expectedColor +
+                        " but got " + actualUsernameColor
+        );
+
+        assertEquals(
+                expectedGrey,
+                actualPasswordColor,
+                "Expected color of the border of the field Придумайте пароль to be " + expectedGrey +
+                        " but got " + actualPasswordColor
+        );
     }
 
     @AfterMethod
     public void afterMethod() {
-        // driver.quit();
+        driver.quit();
     }
 
 }
