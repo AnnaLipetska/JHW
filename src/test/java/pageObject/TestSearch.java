8126abb16b0a594737e119c3faea09b7a8bcbd63
AnnaLipetska <a.lipetska@gmail.com>
2020-05-23 10:16:03 +0300

    Page Object Homework additional check added.

--- src/test/java/pageObject/TestSearch.java
+++ src/test/java/pageObject/TestSearch.java
@@ -1,9 +1,14 @@
 package test.java.pageObject;
 
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import test.java.pageObject.pages.HomePage;
 import test.java.pageObject.pages.SearchPage;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 
@@ -48,5 +53,32 @@ public class TestSearch extends TestBaseSetup {
                 actualNumberLinks,
                 expectedNumberLinks,
                 "Number of the links to be " + expectedNumberLinks + " but got " + actualNumberLinks);
+        //.......
+        // Additional check
+        List<String> expecteList = Arrays.asList(
+                "Смартфоны, ТВ и электроника",
+                "Бытовая техника, интерьер",
+                "Компьютеры и ноутбуки",
+                "Уцененные товары",
+                "Комплектующие и аксессуары для электронных сигарет",
+                "Спорт и увлечения",
+                "Аккумуляторы и зарядные устройства для инструмента и садовой техники",
+                "Клейкая лента, скотч, стрейч пленка",
+                "Аксессуары для видеонаблюдения"
+        );
+
+        String linkTest = null;
+        List<String> acttualList = new ArrayList<>();
+
+        List<WebElement> actualElList = searchPage.getLinksLeft();
+        for (WebElement el : actualElList) {
+            linkTest = el.getText();
+            acttualList.add(linkTest);
+        }
+
+        assertEquals(
+                acttualList,
+                expecteList,
+                "The actual list of the links is not equal to the expected list");
     }
 }
